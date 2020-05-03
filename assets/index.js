cdb7adb11b51f98362a0bdf47100de1e583513c6
toy <escusado@gmail.com>
2020-05-03 14:04:08 -0500

    adds test for content async

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,14 @@
 const App = async () => {
   const branch = "feature.add-more-context";
-  const picUrlTemplate = `https://raw.githubusercontent.com/gdljs/gdljs.github.io/${branch}/`;
-  const picsUrl = `https://api.github.com/repos/gdljs/gdljs.github.io/git/trees/${branch}?recursive=1`;
+  const repoUrlTemplate = `https://raw.githubusercontent.com/gdljs/gdljs.github.io/${branch}/`;
 
+  const contentDoc = "content.html";
+  const request = await fetch(repoUrlTemplate + contentDoc, { method: "get" });
+  const requestReader = await request.body.getReader();
+  const content = await requestReader.read();
+  console.log(">>>>>>>", content.value.toString());
+
+  const picsUrl = `https://api.github.com/repos/gdljs/gdljs.github.io/git/trees/${branch}?recursive=1`;
   let pics;
   const response = await fetch(picsUrl);
   if (response.ok) {
@@ -10,13 +16,15 @@ const App = async () => {
     pics = repoContent.tree
       .filter((item) => item.path.indexOf("assets/pics/") === 0)
       .map((pic) => {
-        return picUrlTemplate + pic.path;
+        return repoUrlTemplate + pic.path;
       });
   } else {
     alert("HTTP-Error: " + response.status);
   }
 
-  console.log(pics);
+  // console.log(pics);
 };
 
-App();
+window.addEventListener("DOMContentLoaded", (event) => {
+  App();
+});
